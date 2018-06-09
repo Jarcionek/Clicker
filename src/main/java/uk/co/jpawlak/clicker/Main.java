c89df032e4bfdc17308839e27c40554af6b40a34
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2018-06-09 15:04:31 +0100

    Application now starts with system's Look and Feel

--- src/main/java/uk/co/jpawlak/clicker/Main.java
+++ src/main/java/uk/co/jpawlak/clicker/Main.java
@@ -5,9 +5,18 @@ import uk.co.jpawlak.clicker.actions.MouseButtonPressAction;
 import uk.co.jpawlak.clicker.actions.MouseButtonReleaseAction;
 import uk.co.jpawlak.clicker.gui.MainWindow;
 
+import javax.swing.UIManager;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         Actions actions = new Actions();
         actions.add(new MouseButtonPressAction(true, false, false));
         actions.add(new MouseButtonReleaseAction(true, false, false));
